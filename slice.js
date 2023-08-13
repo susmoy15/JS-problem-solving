dc354e496d2ebe1e3bdcbdd8ace8639e58b2b36e
susmoy15 <107901530+susmoy15@users.noreply.github.com>
2023-08-13 16:31:46 +0600

    good

---
+++ slice.js
@@ -0,0 +1,13 @@
+// const friends = [23,5,7,89,56,78,6,3,56,76,95];
+  
+// const police = friends.slice(3,7);
+// console.log(police);
+// crossOriginIsolated.log(friends);
+
+
+// splice
+
+const friendsAll = [23,5,7,89,56,78,6,3,56,76,95];
+const police = friendsAll.splice(3,7);
+console.log(police);
+console.log(friendsAll);
